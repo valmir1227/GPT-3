a6cfa26934d24d9afb6b6d4195631fba7469f0c2
Valmir Almeida <almeidavalmir76@gmail.com>
2022-07-26 08:11:52 -0300

    :package: brand finished

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
+
 import "./brand.css";
 
-export const Brand = () => {
-  return <div>Brand</div>;
+const Brand = () => {
+  return (
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={slack} alt="google" />
+      </div>
+      <div>
+        <img src={shopify} alt="google" />
+      </div>
+      <div>
+        <img src={dropbox} alt="google" />
+      </div>
+      <div>
+        <img src={atlassian} alt="google" />
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
