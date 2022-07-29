1153595a771c2afde854800d44537da5bf647b38
Valmir Almeida <almeidavalmir76@gmail.com>
2022-07-29 12:34:28 -0300

    :package: Update Blog and Article 90%

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./article.css";
 
-export const Article = () => {
-  return <div>Article</div>;
+const Article = ({ imgUrl, date, text }) => {
+  return (
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blog image" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{text}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
 };
 
 export default Article;
